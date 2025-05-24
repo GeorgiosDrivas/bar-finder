74fff711c14e6a63aa24da4a46535007b233f1bf
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-05-24 20:17:32 +0300

    Get user location logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+
 import Nav from "@/components/Nav";
 import SearchBar from "@/components/SearchBar";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [userLocation, setUserLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error("Error getting user location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  };
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
   return (
     <>
       <Nav />
